835883c8553955f663b301e6775a709025ca1f7c
Hedygh <dyouahes@gmail.com>
2026-05-15 20:12:06 +0200

    add .json

--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -2,9 +2,9 @@ import fs from 'fs';
 
 export default function readDatabase(path) {
   return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf-8', (err, data) => {
-      if (err) {
-        reject(err);
+    fs.readFile(path, 'utf-8', (error, data) => {
+      if (error) {
+        reject(error);
         return;
       }
 
@@ -12,10 +12,10 @@ export default function readDatabase(path) {
       const students = lines.slice(1);
       const fields = {};
 
-      students.forEach((line) => {
-        const parts = line.split(',');
-        const firstname = parts[0];
-        const field = parts[3];
+      students.forEach((student) => {
+        const studentData = student.split(',');
+        const firstname = studentData[0];
+        const field = studentData[3];
 
         if (!fields[field]) {
           fields[field] = [];
